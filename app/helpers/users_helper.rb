85a3cfec9cb3e676ce6c01ebf46956b8527a0d93
tech.academio <techacademio@gmail.com>
2018-09-29 10:02:16 +0000

    user login

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,5 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
+  end
 end
